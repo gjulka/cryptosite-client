4c38d6971433981114ce53d6c6596a1fe8ca34a3
gjulka <g.julka10@gmail.com>
2019-04-29 21:24:47 -0500

    edit username

--- src/api/api.js
+++ src/api/api.js
@@ -8,9 +8,10 @@ export function getCoinsRequest() {
 }
 
 export function getUserRequest() {
-    const url = 'https://yourcryptoserver.herokuapp.com/'
+    // const url = 'https://yourcryptoserver.herokuapp.com/'
+    const apiUrl = 'http://localhost:5000/'
 
-    const get = axios.get(url, {
+    const get = axios.get(apiUrl, {
         headers: {
             authorization: 'Bearer ' + localStorage.token 
         }
@@ -20,8 +21,10 @@ export function getUserRequest() {
 }
 
 export function getUsersCoinsRequest() {
-    const url = 'https://yourcryptoserver.herokuapp.com/userscoins'
-    const get = axios.get(url, {
+    // const url = 'https://yourcryptoserver.herokuapp.com/userscoins'
+    const apiUrl = 'http://localhost:5000/userscoins'
+
+    const get = axios.get(apiUrl, {
         headers: {
             authorization: 'Bearer ' + localStorage.token 
         }
@@ -30,7 +33,9 @@ export function getUsersCoinsRequest() {
 }
 
 export function signUpUserRequest(body) {
-    const apiUrl = 'https://yourcryptoserver.herokuapp.com/auth/signup'
+    // const apiUrl = 'https://yourcryptoserver.herokuapp.com/auth/signup'
+    const apiUrl = 'http://localhost:5000/auth/signup'
+
 
     const post = axios(apiUrl, {
         method: 'post',
@@ -44,7 +49,9 @@ export function signUpUserRequest(body) {
 }
 
 export function loginUserRequest(body) {
-    const apiUrl = 'https://yourcryptoserver.herokuapp.com/auth/login'
+    // const apiUrl = 'https://yourcryptoserver.herokuapp.com/auth/login'
+    const apiUrl = 'http://localhost:5000/auth/login'
+
 
     const post = axios(apiUrl, {
         method: 'post',
@@ -57,8 +64,27 @@ export function loginUserRequest(body) {
     return post
 }
 
+export function updateUserRequest(body) {
+    // const apiUrl = 'https://yourcryptoserver.herokuapp.com/auth/login'
+    const apiUrl = 'http://localhost:5000/auth/update'
+
+
+    const post = axios(apiUrl, {
+        method: 'post',
+        data: JSON.stringify(body),
+        headers: {
+            'content-type': 'application/json',
+            authorization: 'Bearer ' + localStorage.token
+        }
+    })
+
+    return post
+}
+
 export function postUsersCoinsRequest(body) {
-    const apiUrl = 'https://yourcryptoserver.herokuapp.com/userscoins'
+    // const apiUrl = 'https://yourcryptoserver.herokuapp.com/userscoins'
+    const apiUrl = 'http://localhost:5000/userscoins'
+
     const post = axios(apiUrl, {
         method: 'post',
         data: JSON.stringify(body),
@@ -72,7 +98,9 @@ export function postUsersCoinsRequest(body) {
 }
 
 export function postDeleteUsersCoinsRequest(body) {
-    const apiUrl = 'https://yourcryptoserver.herokuapp.com/userscoins/delete'
+    // const apiUrl = 'https://yourcryptoserver.herokuapp.com/userscoins/delete'
+    const apiUrl = 'http://localhost:5000/userscoins/delete'
+
     const post = axios(apiUrl, {
         method: 'post',
         data: JSON.stringify(body),
--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import classes from './User.module.css'
-import { getUserRequest } from '../../api/api'
+import { getUserRequest, updateUserRequest } from '../../api/api'
 
 import editButton from '../../assets/pencil-edit-button.svg'
 
@@ -10,6 +10,8 @@ class User extends Component {
 
         this.editUsername = this.editUsername.bind(this)
         this.editPassword = this.editPassword.bind(this)
+        this.onSaveUsername = this.onSaveUsername.bind(this)
+        this.onSavePassword = this.onSavePassword.bind(this)
 
         this.state = {
             username: '',
@@ -36,6 +38,28 @@ class User extends Component {
         this.setState({
             usernameEdit: !this.state.usernameEdit
         })
+
+        const body = {id: this.state.id}
+
+        updateUserRequest(body)
+        .then(result => {
+            console.log(result);
+        })
+    }
+
+    onSaveUsername() {
+        console.log('saved username');
+        this.setState({
+            usernameEdit: !this.state.usernameEdit
+        })
+
+        const body = {username: document.querySelector('#username').innerText}
+
+        updateUserRequest(body)
+            .then(result => {
+                console.log(result);
+            })
+
     }
 
     editPassword() {
@@ -44,12 +68,26 @@ class User extends Component {
         })
     }
 
+    onSavePassword() {
+        console.log('saved password');
+        this.setState({
+            passwordEdit: !this.state.passwordEdit
+        })
+    }
+
     render() {
         var usernameStyle 
         var passwordStyle
+        var hideEditUsername
+        var hideEditPassword
+        var saveUsername
+        var savePassword
+
+
 
         if (this.state.usernameEdit === false) {
             usernameStyle = null
+            saveUsername = {display: 'none'}
         } else {
             usernameStyle = {
                 border: '1px solid black',
@@ -57,16 +95,24 @@ class User extends Component {
                 backgroundColor: 'white',
                 padding: '0 5px'
             }
+
+            hideEditUsername = {
+                display: 'none'
+            }
         }
 
         if (this.state.passwordEdit === false) {
             passwordStyle = null
+            savePassword = {display: 'none'}
         } else {
             passwordStyle = {
                 border: '1px solid black',
                 borderRadius: '4px',
                 backgroundColor: 'white',
                 padding: '0 5px'
+            }
+            hideEditPassword = {
+                display: 'none'
             }
         }
 
@@ -76,14 +122,17 @@ class User extends Component {
                     <h1>User Info</h1>
                     <div className={classes.Username}>
                         <h2>Username:</h2>
-                        <h3 contentEditable={this.state.usernameEdit} style={usernameStyle}>{this.state.username}</h3>
-                        <button onClick={this.editUsername}><img src={editButton} alt=''></img></button>
+                        <h3 id='username' contentEditable={this.state.usernameEdit} style={usernameStyle}>{this.state.username}</h3>
+                        <button style={hideEditUsername} onClick={this.editUsername}><img src={editButton} alt=''></img></button>
+                        <button style={saveUsername} onClick={this.onSaveUsername}>Save</button>
+
                     </div>
 
                     <div className={classes.Password}>
                         <h2>Password:</h2>
-                        <h3 contentEditable={this.state.passwordEdit} style={passwordStyle}>{this.state.password}</h3>
-                        <button onClick={this.editPassword}><img src={editButton} alt=''></img></button>
+                        <h3 id='password' contentEditable={this.state.passwordEdit} style={passwordStyle}>{this.state.password}</h3>
+                        <button style={hideEditPassword} onClick={this.editPassword}><img src={editButton} alt=''></img></button>
+                        <button style={savePassword} onClick={this.onSavePassword}>Save</button>
                     </div>
                 </div>
             </div>
